03e7be6c8e1c9fb7f8b220defef6c0187a6383bd
Eugen Rata <eugen.rata@gmail.com>
2024-06-07 11:04:28 -0500

    Update Card.js

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './Card.css'
 
-const Card = (props) => {
-    const classes = ' card ' + props.className;
+const Card = ({className}) => {
+    const classes = ' card ' + className;
 
     return <div className={classes}>{props.children}</div>;
 };
+
+Card.propTypes = {
+    className:  PropTypes.string,
+}
 
 export default Card;
